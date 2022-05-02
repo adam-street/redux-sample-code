1844ce21e47bc7cee07a9928ab80c7ecb2fe6b71
adam-street <adam.street.dev@gmail.com>
2022-05-02 17:26:07 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {ADD, LOGIN} from "./modules/calendar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const dispatch = useDispatch();
+  const user_list = useSelector((state) => { return state.user_list})
+  const isLoggedIn = useSelector((state) => { return state.isLoggedIn})
+
+  function add() {
+    dispatch({
+      type: ADD,
+      foo: {
+        username: 'user3',
+        password: 'pass1'
+      }
+    })
+  }
+
+  function find() {
+    dispatch({
+      type: LOGIN,
+      creds: {
+        username: 'user3',
+        password: 'pass1'
+      }
+    })
+  }
+
+  return <>
+    <button onClick={add}>add</button>
+    <button onClick={find}>find</button>
+    {/*{user_list.map(u => <h1>{u.username}</h1>)}*/}
+    <h1>is logged in: {isLoggedIn ? 'true' : 'false'}</h1>
+  </>
 }
 
 export default App;
